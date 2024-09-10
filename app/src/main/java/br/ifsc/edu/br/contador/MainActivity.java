3272ddc71b0bdd99f8e01e62ea47beea124cb1a4
diego anderson <diegoandersoncarva@hotmail.com>
2024-09-09 22:15:56 -0300

    commit

--- app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button myBtn = findViewById(R.id.button);
-        myBtn.setText("xas");
+        TextView tv = findViewById(R.id.textView);
+        EditText edMin =findViewById(R.id.edMin);
+        EditText edMax =findViewById(R.id.edMax);
+        myBtn.setText("Me aperta");
         myBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Button b=(Button) view;
-                b.setText("0");
+                TextView t=(TextView) view;
+                Random random = new Random();
+                int min = (Integer.parseInt(edMin.getText().toString()));
+                int max = (Integer.parseInt(edMax.getText().toString()));
+                t.setText(Integer.toString(random.nextInt(min-max)+min));
             }
         });
     }
