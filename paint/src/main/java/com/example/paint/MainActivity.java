aa34042e8727b191702722a271d058d180dad140
diegoandersonn <diegoandersoncarva@hotmail.com>
2024-10-07 21:49:52 -0300

    color picker adicionado

--- paint/src/main/java/com/example/paint/MainActivity.java
+++ paint/src/main/java/com/example/paint/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.skydoves.colorpickerview.ColorEnvelope;
+import com.skydoves.colorpickerview.ColorPickerDialog;
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 
 public class MainActivity extends AppCompatActivity {
+        SimplePaint simplePaint;
+        Button colorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simplePaint = findViewById(R.id.simplePaint2);
+        colorButton = findViewById(R.id.button);
+        colorButton.setOnClickListener(view -> {
+            new ColorPickerDialog.Builder(this)
+                    .setTitle("ColorPicker Dialog")
+                    .setPreferenceName("MyColorPickerDialog")
+                    .setPositiveButton("Confirmado", new ColorEnvelopeListener() {
+                @Override
+                public void onColorSelected(ColorEnvelope envelope, boolean fromUser) {
+                    setColor(envelope);
+                }
+            }).setNegativeButton("Cancelado",
+          new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+          }).attachAlphaSlideBar(true) // the default value is true.
+      .attachBrightnessSlideBar(true)  // the default value is true.
+      .setBottomSpace(12) // set a bottom space between the last slidebar and buttons.
+      .show();
+        });
+    }
+    private void setColor(ColorEnvelope envelope) {
+        simplePaint.myPaint.setColor(envelope.getColor());
+        colorButton.setBackgroundColor(envelope.getColor());
     }
 }
